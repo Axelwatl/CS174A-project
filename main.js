6fe6c13ca90979be2f5e340bafb7b3a838e8a070
cyrussori <soribellocj@gmail.com>
2024-11-13 01:18:34 -0800

    Feature: Pause

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 import * as THREE from 'three';
 import { OrbitControls  } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FontLoader } from 'three/examples/jsm/Addons.js';
+import { TextGeometry } from 'three/examples/jsm/Addons.js';
 import { genMaze  } from './maze';
 
-const scene = new THREE.Scene();
+let gameScene = new THREE.Scene();
+let menuScene = new THREE.Scene();
+let currentScene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+currentScene = gameScene;
+
+let menuCameraTarget = new THREE.Vector3(0, 150, 0);
+let camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+let menuCamera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500);
+menuCamera.position.set(0, 400, 700);
+menuCamera.lookAt(menuCameraTarget);
+
 const orbit_camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let current_camera = camera;
 
@@ -22,7 +33,7 @@ let floor_material = new THREE.MeshStandardMaterial({ map: floor_texture });
 let floor = new THREE.Mesh(new THREE.PlaneGeometry(60, 60), floor_material);
 floor.rotateX(-Math.PI / 2);
 
-scene.add(floor);
+gameScene.add(floor);
 
 const createAxisLine = (color, start, end) => {
     const geometry = new THREE.BufferGeometry().setFromPoints([start, end]);
@@ -32,34 +43,34 @@ const createAxisLine = (color, start, end) => {
 const xAxis = createAxisLine(0xff0000, new THREE.Vector3(0, 0, 0), new THREE.Vector3(3, 0, 0)); // Red
 const yAxis = createAxisLine(0x00ff00, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 3, 0)); // Green
 const zAxis = createAxisLine(0x0000ff, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 3)); // Blue
-scene.add(xAxis);
-scene.add(yAxis);
-scene.add(zAxis);
+gameScene.add(xAxis);
+gameScene.add(yAxis);
+gameScene.add(zAxis);
 
 const pointLight = new THREE.PointLight(0xffffff, 100, 100);
 pointLight.position.set(5, 5, 5); // Position the light
-scene.add(pointLight);
+gameScene.add(pointLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0.5, .0, 1.0).normalize();
-scene.add(directionalLight);
+gameScene.add(directionalLight);
 
 const ambientLight = new THREE.AmbientLight(0x505050);  // Soft white light
-scene.add(ambientLight);
+gameScene.add(ambientLight);
 
 // Make player
 const player_geometry = new THREE.SphereGeometry(0.5, 14, 14);
 const player_material = new THREE.MeshStandardMaterial({ color: 0x000fff });
 const player = new THREE.Mesh(player_geometry, player_material);
-scene.add(player);
+gameScene.add(player);
 
 // Temp. use box for hand (MC)
 const hand_geometry = new THREE.BoxGeometry(0.2, 0.2, 0.5);
 const hand_material = new THREE.MeshStandardMaterial({ color: 0x804fff });
 const hand = new THREE.Mesh(hand_geometry, hand_material);
 hand.position.set(0.3, -0.2, -0.5); // Relative to the camera
-camera.add(hand); // Fix to player
-scene.add(camera);
+gameScene.add(hand); // Fix to player
+gameScene.add(camera);
 
 
 const phong_material = new THREE.MeshPhongMaterial({
@@ -68,7 +79,7 @@ const phong_material = new THREE.MeshPhongMaterial({
 });
 
 const grid = new THREE.GridHelper(60, 30);
-scene.add(grid);
+gameScene.add(grid);
 
 let mapSize = Math.floor(Math.random() * (34 - 15 + 1) + 15);
 if (mapSize % 2 == 0) {
@@ -78,9 +89,66 @@ if (mapSize % 2 == 0) {
         mapSize += 1;
     }
 }
-console.log(mapSize);
+
 const map = Array.from({ length: mapSize }, () => new Array(mapSize).fill(1))
-genMaze(map, scene);
+genMaze(map, gameScene);
+
+
+function updateCameraPosition() {
+    camera.position.set(player.position.x, player.position.y + 1.5, player.position.z); 
+    camera.lookAt(player.position.x, player.position.y + 1.5, player.position.z + 1);
+}
+
+let group, textMesh, textMesh2, textMesh3, textGeo, textGeo2, textGeo3, font;
+
+const menuDirLight = new THREE.DirectionalLight(0xffffff, 1);
+menuDirLight.position.set(0,0,1).normalize();
+menuScene.add(menuDirLight);
+
+const menuPtLight = new THREE.PointLight(0xffffff, 1, 0, 0);
+menuPtLight.color.setHSL(Math.random(), 1, 0.5);
+menuPtLight.position.set(0, 100, 90);
+menuScene.add(menuPtLight);
+
+let materials = [
+    new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ),
+    new THREE.MeshPhongMaterial({ color: 0xffffff })
+];
+
+group = new THREE.Group();
+group.position.y = 50;
+menuScene.add(group);
+
+loadFont();
+
+function loadFont() {
+    const loader = new FontLoader();
+    loader.load('fonts/Pixelify.json', (response) => {
+        font = response;
+        createMenuText();
+    });
+}
+
+function createMenuText() {
+    if (!font) return;
+
+    textGeo = new TextGeometry('PAUSED ||', {
+        font: font,
+        size: 50,
+        depth: 30,
+        curveSegments: 3,
+        bevelThickness: 2,
+        bevelSize: 1,
+        bevelEnabled: true
+    })
+
+    textGeo.computeBoundingBox();
+    const centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+
+    textMesh = new THREE.Mesh(textGeo, materials);
+    textMesh.position.set(centerOffset, 100, 0);
+    group.add(textMesh);
+}
 
 window.addEventListener('keydown', function(event) {
     const speed = 0.5;
@@ -114,6 +182,14 @@ window.addEventListener('keydown', function(event) {
             current_camera.lookAt(0, 0, 0);
             controls.object = current_camera;
             break;
+        case 'Escape':
+            currentScene = (currentScene === gameScene) ? menuScene : gameScene;
+            if (currentScene === gameScene) {
+                current_camera = camera;
+            } else {
+                current_camera = menuCamera;
+            }
+            break;
     }
 
     // TBD : Fix collision check, prob push the player back the same units if collision detected
@@ -124,19 +200,11 @@ window.addEventListener('keydown', function(event) {
         player.position.set(newX, player.position.y, newZ);
     //}
 });
-
-function updateCameraPosition() {
-    camera.position.set(player.position.x, player.position.y + 1.5, player.position.z); 
-    camera.lookAt(player.position.x, player.position.y + 1.5, player.position.z + 1);
-}
-
-
 function animate() {
     requestAnimationFrame(animate);
     
     // Move camera to follow the player's position
     updateCameraPosition();
-
-    renderer.render(scene, current_camera);
+    renderer.render(currentScene, current_camera);
 }
 animate();
